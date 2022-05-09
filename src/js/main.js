e02b8df3e34addabed7467be07d50f6aabec4ed3
AyrtonRada <axel582011@hotmail.com>
2022-05-09 19:32:37 -0300

    insercion de videos e imagenes

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,59 @@ moreOptions.addEventListener('click', (e)=>{
     e.preventDefault()
     moreMenu.classList.toggle('show')
 })
+
+const videos = [
+    { id: "PyMlV5_HRWk"},
+    { id: "XCbMVbeKlCg"},
+    { id: "Fmdb-KmlzD8"},
+    { id: "lOthvD1rMbQ"},
+    { id: "nXDk86lQhto"}
+]
+
+const sliderContainer = document.querySelector("#slider")
+const currentContainer = document.querySelector("#current")
+const videosContainer = document.querySelector("#videos-container")
+const bNext = document.querySelector("#next")
+const bPrev = document.querySelector("#prev")
+
+
+let current = 0
+
+renderCurrentVideo(videos[current].id)
+
+//boton next para evitar que siga avanzando si ya no hay videos
+bNext.addEventListener("click", (e) => {
+   let changed = current
+    current = current + 1 < videos.length ? current + 1 : current
+    if(current !== changed){
+        renderCurrentVideo(videos[current].id)
+    }
+})
+
+// boton prev siempre y cuando exista 1 video 
+bPrev.addEventListener("click", (e) => {
+    let changed = current
+    current = current - 1 >= 0 ? current - 1 : current
+    if(current !== changed){
+        renderCurrentVideo(videos[current].id)
+    }
+})
+
+function renderCurrentVideo(id) {
+    currentContainer.innerHTML = `<iframe width="100%" height="720" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function renderVideos(){
+    const html = videos.map((video) => {
+        return `
+        <div class="item">
+            <a href="#">
+                <img src="http://i3.ytimg.com/vi/${video.id}/hqdefault.jpg" />
+            </a>
+        </div>`
+    })
+
+    videosContainer.innerHTML = html.join("")
+}
+
+renderVideos()
